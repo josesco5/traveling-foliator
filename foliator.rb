7c52cecafdded862a7205457183cc68bfa064d72
José Escorche <josesco5@gmail.com>
2018-03-01 11:47:16 -0300

    Receive params to foliate remote pdf

--- foliator.rb
+++ foliator.rb
@@ -1,18 +1,30 @@
 #!/usr/bin/env ruby
 require 'json'
 require 'document/foliator'
-require 'combine_pdf'
-require 'open-uri'
-
-puts JSON.parse('{ "we" : "love", "using" : "Lambda" }')
 
 if ARGV.length > 0
-  options = JSON.parse( ARGV[0] )
+  options = JSON.parse( ARGV[0], {symbolize_names: true} )
   puts options
+  file_url = options[:file_url]
+  start_folio = options[:start_folio] || 1
+  just_folios = options[:just_folios]
+  pixels_to_left = options[:pixels_to_left] || 0
+  double_sided = options[:double_sided]
+  every_other_page = options[:every_other_page]
+  stamp_url = options[:stamp_url]
+  issue_date = options[:issue_date]
+
+  if file_url
+    foliated_pdf = Document::Foliator.foliate_from_url(
+      file_url, start_folio, just_folios, pixels_to_left, double_sided,
+      every_other_page, stamp_url, issue_date
+    )
+    if foliated_pdf
+      puts "File at #{file_url}. Pages of foliated document: #{foliated_pdf.pages.count}"
+    else
+      puts "File at #{file_url} could not be foliated"
+    end
+  end
 end
 
-file_url = 'https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf'
-file = open file_url
-pdf = CombinePDF.parse file.read
-foliated_pdf = Document::Foliator.foliate(pdf, 1, false, 0, false, false)
-puts "Pages of foliated document: #{foliated_pdf.pages.count}"
+puts 'Finished'
