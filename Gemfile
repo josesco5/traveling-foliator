00b6ded7012b9f3856c4e4ee454173fc2758cc3a
José Escorche <josesco5@gmail.com>
2018-03-01 08:59:40 -0300

    Add document-foliator gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,12 @@ source 'https://rubygems.org'
 
 gem 'json', '1.8.2'
 
+# To generate packaged version (Comment below)
+gem 'document-foliator', '0.1.0', :path => '[ABSOLUTE_PATH_TO_THIS_PROJECT]/local-gems/document-foliator-0.1.0'
+
+# To run packaged version: (Comment above)
+#gem 'document-foliator', '0.1.0', :path => './local-gems/document-foliator-0.1.0'
+
 group :development do
   gem 'rake'
 end
--- foliator.rb
+++ foliator.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'document/foliator'
+require 'combine_pdf'
+require 'open-uri'
 
 puts JSON.parse('{ "we" : "love", "using" : "Lambda" }')
+
+file_url = 'https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf'
+file = open file_url
+pdf = CombinePDF.parse file.read
+foliated_pdf = Document::Foliator.foliate(pdf, 1, false, 0, false, false)
+puts "Pages of foliated document: #{foliated_pdf.pages.count}"
