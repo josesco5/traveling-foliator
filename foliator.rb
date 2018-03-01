0c563da384eca544598c80779bb755f94f9d1903
José Escorche <josesco5@gmail.com>
2018-03-01 09:37:20 -0300

    Make generated script abled to receive params

--- foliator.rb
+++ foliator.rb
@@ -6,6 +6,11 @@ require 'open-uri'
 
 puts JSON.parse('{ "we" : "love", "using" : "Lambda" }')
 
+if ARGV.length > 0
+  options = JSON.parse( ARGV[0] )
+  puts options
+end
+
 file_url = 'https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf'
 file = open file_url
 pdf = CombinePDF.parse file.read
